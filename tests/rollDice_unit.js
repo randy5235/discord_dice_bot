5f03fc2cfba8c944ab31a2b288139a6aacb197b7
Randy Wressell <rwressell@globesherpa.com>
2016-12-02 10:39:48 -0800

    Add tests

--- tests/rollDice_unit.js
+++ tests/rollDice_unit.js
@@ -19,3 +19,15 @@ describe(`rollDice function`, function (){
     expect(rollDice(0)).to.equal(0);
   });
 });
+
+  describe(`Determine randomness of rollDice function`, function() {
+    it(`rolls 1d6 60000000 times and captures number frequency`, function() {
+      this.timeout(100000);
+      var outputArray = [0,0,0,0,0,0];
+      for (let i = 0; i <= 60000000; i++) {
+        let value = rollDice(6)-1;
+        outputArray[value] += 1;
+      }
+      console.log(outputArray);
+    });
+  });
