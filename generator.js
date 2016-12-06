92a608ebc164201fdfea097402f2f6a26f4ac133
Randy Wressell <rwressell@globesherpa.com>
2016-12-05 22:50:30 -0800

    Update some logic

--- generator.js
+++ generator.js
@@ -5,9 +5,9 @@ module.exports.diceRoller = function(message) {
   //this should take expressions like 3d6 etc.
   var diceExpression = message.replace(/^\/r |^\/roll /,'');
   console.log(diceExpression);
-  var count = diceExpression.match(/^[^d]*/)[0] || 1;
+  var count = diceExpression.match(/^[^d]*/)[0] ? diceExpression.match(/^[^d]*/)[0] : 1;
   // console.log(count);
-  var die = diceExpression.match(/d(.[^+\-*\/\s]*)/)[1] || 0;
+  var die = diceExpression.match(/d(.[^+\-*\/\s]*)/)[1]  ? diceExpression.match(/d(.[^+\-*\/\s]*)/)[1] : 0;
   var operator = diceExpression.match(/[\+\-\/\*]/) ? diceExpression.match(/[\+\-\/\*]/)[0] : 0;
   var operand = diceExpression.match(/[\+\-\/\*](.*)/) ? diceExpression.match(/[\+\-\/\*](.*)/)[1] : 0;
   var diceArray = [];
