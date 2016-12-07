da4bd4bb0056ece5cad8cbd058212720550d586c
Randy Wressell <rwressell@globesherpa.com>
2016-12-06 23:02:34 -0800

    Add more tests

--- tests/diceRoller_unit.js
+++ tests/diceRoller_unit.js
@@ -2,10 +2,20 @@
 const expect = require('chai').expect;
 const diceRoller = require('../generator').diceRoller;
 
-describe('The Dice Roller takes expression', function(){
-  it('1d6 should return a value', function(){
-    var value = diceRoller('1d6')[0];
-    console.log(`output from diceRoller`, value);
-    expect(value).to.be.within(1,6);
-  })
+describe('The Dice Roller takes basic dice expressions', function(){
+  it('1d6 should return a value between 1 and 6', function(){
+    var value = diceRoller('1d6');
+    console.log(`output from diceRoller`, value[value.length-1]);
+    expect(value[value.length-1]).to.be.within(1,6);
+  });
+  it('2d6 should return a value between 2 and 12', function(){
+    var value = diceRoller('2d6');
+    console.log(`output from diceRoller`, value[value.length-1]);
+    expect(value[value.length-1]).to.be.within(2,12);
+  });
+  it('d6 should return a value between 1 and 6', function(){
+    var value = diceRoller('2d6');
+    console.log(`output from diceRoller`, value[value.length-1]);
+    expect(value[value.length-1]).to.be.within(1,6);
+  });
 });
