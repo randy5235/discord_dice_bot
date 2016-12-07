22ce6280b62bd0dbeb63557db3e1378c69ecb3aa
Randy Wressell <rwressell@globesherpa.com>
2016-12-06 23:51:15 -0800

    Update Tests

--- tests/diceRoller_unit.js
+++ tests/diceRoller_unit.js
@@ -5,17 +5,23 @@ const diceRoller = require('../generator').diceRoller;
 describe('The Dice Roller takes basic dice expressions', function(){
   it('1d6 should return a value between 1 and 6', function(){
     var value = diceRoller('1d6');
-    console.log(`output from diceRoller`, value[value.length-1]);
-    expect(value[value.length-1]).to.be.within(1,6);
+    console.log(`output from diceRoller`, value);
+    expect(value.match(/.*= (.*)/)[1]).to.be.within(1,6);
   });
   it('2d6 should return a value between 2 and 12', function(){
     var value = diceRoller('2d6');
-    console.log(`output from diceRoller`, value[value.length-1]);
-    expect(value[value.length-1]).to.be.within(2,12);
+    console.log(`output from diceRoller`, value);
+    expect(value.match(/.*= (.*)/)[1]).to.be.within(2,12);
   });
   it('d6 should return a value between 1 and 6', function(){
-    var value = diceRoller('2d6');
-    console.log(`output from diceRoller`, value[value.length-1]);
-    expect(value[value.length-1]).to.be.within(1,6);
+    var value = diceRoller('d6');
+    // console.log(`output from diceRoller`, value, value.match(/.*= (.*)/)[1]);
+    expect(value.match(/.*= (.*)/)[1]).to.be.within(1,6);
+  });
+  it('d% should return a value between 10 and 100 in 10s', function(){
+    var value = diceRoller('d%');
+    console.log(`output from diceRoller`, value);
+    expect(value.match(/.*= (.*)/)[1]).to.be.within(10,100);
+    expect(value.match(/.*= (.*)/)[1] % 10).to.equal(0);
   });
 });
--- tests/rollDice_unit.js
+++ tests/rollDice_unit.js
@@ -23,9 +23,31 @@ describe(`rollDice function`, function (){
   describe(`Determine randomness of rollDice function`, function() {
     it(`rolls 1d6 60000000 times and captures number frequency`, function() {
       this.timeout(100000);
-      var outputArray = [0,0,0,0,0,0];
-      for (let i = 0; i <= 60000000; i++) {
+      var outputArray = new Array(6);
+      outputArray.fill(0);
+      for (let i = 0; i <= 6000000; i++) {
         let value = rollDice(6)-1;
+        // console.log(value);
+        outputArray[value] += 1;
+      }
+      console.log(outputArray);
+    });
+    it(`rolls 1d% 10000000 times and captures number frequency`, function() {
+      this.timeout(100000);
+      var outputArray = new Array(10);
+      outputArray.fill(0);
+      for (let i = 0; i <= 10000000; i++) {
+        let value = rollDice('%')/10 -1;
+        outputArray[value] += 1;
+      }
+      console.log(outputArray);
+    });
+    it(`rolls 1d100 100000000 times and captures number frequency`, function() {
+      this.timeout(100000);
+      var outputArray = new Array(100);
+      outputArray.fill(0);
+      for (let i = 0; i <= 100000000; i++) {
+        let value = rollDice(100)-1;
         outputArray[value] += 1;
       }
       console.log(outputArray);
